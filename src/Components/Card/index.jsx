a7d2ef65b4b74172f3d4cc6a1e7cc5cd7de1c96c
eduardonk9999 <thiago.novak7@gmail.com>
2021-03-12 16:56:01 -0300

    Add, requisicao

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -7,41 +7,54 @@ import { Container, CartdItem } from './style';
 export default function Card() {
   const [pokemons, setPokemons] = useState([]);
   const [infosPokemons, setinfosPokemons] = useState([]);
-  const [types, setTypes] = useState([]);
+  // const [types, setTypes] = useState([]);
 
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/`)
       .then(response => response.json())
       .then(data => {
-        setPokemons(data.results);
+        setPokemons(data.results)
+
+        data.results.map(names => {
+          // console.log(names.url)
+          fetch(`${names.url}`)
+            .then(data => data.json())
+            .then(dataPoke => {
+              setinfosPokemons(dataPoke)
+            })
+        })
       })
   }, [])
 
 
-  useEffect(() => {
-    let id;
-    for (id = 0; id < 20; id++) {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${id + 1}/`)
-        .then(data => data.json())
-        .then(dataPoke => {
-          setinfosPokemons(dataPoke)
-        })
-    }
-  }, [])
+  // useEffect(() => {
+  //   let id;
+  //   for (id = 0; id < 20; id++) {
+  //     fetch(`https://pokeapi.co/api/v2/pokemon/${id + 1}/`)
+  //       .then(data => data.json())
+  //       .then(dataPoke => {
+  //         dataPoke.types.map(typesInfos => (
+  //           console.log(typesInfos.type.name)
+  //           // setinfosPokemons()
+  //         ))
+  //       })
+  //   }
+  // }, [])
 
- 
 
-  console.log(infosPokemons)
+
+
+
 
   // setTypes(types)
 
   return (
     <Container>
-      {pokemons.map(name => (
-        <CartdItem key={name.name}>
-          <h2>{name.name}</h2>
-       
+      {pokemons.map(namePok => (
+        <CartdItem>
+          <h2>{namePok.name}</h2>
+          <span>{console.log(infosPokemons.types)}</span>
         </CartdItem>
       ))}
       
